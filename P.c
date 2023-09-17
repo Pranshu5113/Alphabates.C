bb002f34bb05a54ca37f944def86baa8f318bfd0
Pranshu Singh Patel <116775668+Pranshu5113@users.noreply.github.com>
2023-09-17 09:52:20 +0530

    Add files via upload

---
+++ P.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+main()
+{
+    int i,j;
+    
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+            if(i==1&&j!=5||(i==2&&j!=2&&j!=4&&j!=3)||i==3&&j!=5||i==4&&j==1||i==5&&j==1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        printf("\n");    
+    }
+    
+}    
---
+++ T.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+    int i,j;
+    
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+            if(i==1||j==3)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        printf("\n");    
+    }
+}    
